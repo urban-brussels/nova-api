3d7e8b1fe4ab8a41726f18c10f543cd2cdafb370
Bruno Veyckemans <bveyckemans@cirb.brussels>
2022-01-13 09:32:56 +0100

    filter inquiry

--- src/Permits.php
+++ src/Permits.php
@@ -15,7 +15,6 @@ class Permits
     private string $layer;
     private string $cql_filter;
     private array $results;
-    private string $filter;
     private string $type;
 
     public function __construct(string $type)
@@ -39,6 +38,18 @@ class Permits
         return $this;
     }
 
+    public function filterByInquiryDate(string $date = null): self
+    {
+        if($this->type === "PE") {
+            $this->cql_filter = "date_debut_mpp <= '" . date("Y-m-d") . "T23:59:59Z' AND date_fin_mpp >= '" . date("Y-m-d") . "T00:00:00Z' AND date_debut_mpp >= '" . date("Y-m-d", strtotime("-40 days")) . "T10:00:00Z' AND date_fin_mpp <= '" . date("Y-m-d", strtotime("40 days")) . "T10:00:00Z'";
+        }
+        else {
+            $this->cql_filter = "datedebutmpp <= '" . date("Y-m-d") . "T23:59:59Z' AND datefinmpp >= '" . date("Y-m-d") . "T00:00:00Z' AND datedebutmpp >= '" . date("Y-m-d", strtotime("-40 days")) . "T10:00:00Z' AND datefinmpp <= '" . date("Y-m-d", strtotime("40 days")) . "T10:00:00Z'";
+        }
+
+        return $this;
+    }
+
     public function getResults(): self
     {
         $wfs = new WfsLayer($this->path, $this->layer);
@@ -55,4 +66,8 @@ class Permits
         return $this->results[0] ?? [];
     }
 
+    public function all(): array
+    {
+        return $this->results ?? [];
+    }
 }
