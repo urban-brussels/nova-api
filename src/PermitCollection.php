75b2c78289f226f0790701b84905bb0479bf3601
Bruno Veyckemans <bveyckemans@cirb.brussels>
2022-01-31 10:44:08 +0100

    refactoring

--- src/PermitCollection.php
+++ src/PermitCollection.php
@@ -2,14 +2,19 @@
 
 namespace UrbanBrussels\NovaApi;
 
+use DateTime;
+use DateTimeZone;
+use ici\ici_tools\WfsLayer;
+
 class PermitCollection implements \Iterator
 {
-
+    private PermitQuery $permit_query;
     public array $permits;
     protected int $position = 0;
 
-    public function __construct()
+    public function __construct(PermitQuery $permit_query)
     {
+        $this->permit_query = $permit_query;
         $this->permits = [];
     }
 
@@ -52,5 +57,168 @@ class PermitCollection implements \Iterator
     public function rewind(): void
     {
         $this->position = 0;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function getResults(): self
+    {
+        $wfs = new WfsLayer($this->permit_query->path, $this->permit_query->layer);
+        $wfs->setCqlFilter($this->permit_query->cql_filter)
+            ->setCount($this->permit_query->limit);
+
+        if(!empty($this->order)) {
+            $wfs->setSortBy($this->order[0], $this->order[1]);
+        }
+
+        $results = $wfs->getPropertiesArray(false);
+
+        foreach ($results as $result) {
+            $permit = new Permit($result[$this->permit_query->contextAttribute(Attribute::REFERENCE_NOVA)]);
+            $permit->setLanguage($result[$this->permit_query->contextAttribute(Attribute::LANGUAGE)]);
+            $permit->setType($this->permit_query->type);
+            $permit->setDateSubmission($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_SUBMISSION)]));
+            $permit->setDateArc($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_ARC)]));
+            $permit->setDateAri($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_ARI)]));
+            $permit->setDateAdditionalElements($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_ADDITIONAL_ELEMENTS)]));
+            $permit->setDateCc($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_CC)]));
+            $permit->setDateInquiryBegin($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_INQUIRY_BEGIN)]));
+            $permit->setDateNotification($this->permit_query::toDatetime($result[$this->permit_query->contextAttribute(Attribute::DATE_NOTIFICATION)]));
+            $permit->setAreaTypology($this->defineAreaTypologyFromAttributes($result));
+            $permit->setAdvices($this->defineAdvicesFromAttributes($result));
+            $permit->setAddress($this->defineAddressFromAttributes($result));
+            $permit->setSource($this->defineSource($permit->getReferenceNova()));
+            $permit->setSuspensions($this->defineSuspensions($result['suspensions'] ?? null));
+
+            $this->permits->addPermit($permit);
+        }
+
+        return $this;
+    }
+
+
+    private function defineAreaTypologyFromAttributes(array $attributes): array
+    {
+        if ($this->permit_query->type === "PE") {
+            return [];
+        }
+        $typology = [];
+
+        foreach ($attributes as $k => $v) {
+            if (!is_null($v) && (str_contains($k, 'autorized') || str_contains($k, 'existing') || str_contains(
+                        $k,
+                        'projected'
+                    ))) {
+                $type = str_replace(['autorized', 'existing', 'projected'], '', $k);
+                $subtype = str_replace($type, '', $k);
+                $typology[$type][$subtype] = $v;
+            }
+        }
+
+        return $typology;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    private function defineAdvicesFromAttributes(array $attributes): array
+    {
+        $advices = [];
+
+        $advices['college'] = $attributes['avis_cbe'] ?? $attributes['aviscbe'];
+        $advices['cc'] = $attributes['avis_cc'] ?? $attributes['aviscc'];
+        $advices['fd'] = $attributes['avis_fd'] ?? $attributes['avisfd'];
+
+
+        $json_advices = $attributes['avis_instances'] ?? null;
+        if (is_null($json_advices)) {
+            return $advices;
+        }
+        $json_advices = json_decode($json_advices, true, 512, JSON_THROW_ON_ERROR);
+
+        $instances['fr'] = [];
+        $instances['nl'] = [];
+
+        foreach ($json_advices['data']['Case_Advice_Instance_List']['elements'] as $instance) {
+            $instances['fr'][] = $instance['translations'][0]['label'];
+            $instances['nl'][] = $instance['translations'][1]['label'];
+        }
+
+        return $advices['instances'] = $instances;
+    }
+
+    public function defineAddressFromAttributes(array $attributes): array
+    {
+        $address['streetname']['fr'] = $attributes[$this->permit_query->contextAttribute(Attribute::STREET_NAME_FR)] ?? null;
+        $address['streetname']['nl'] = $attributes[$this->permit_query->contextAttribute(Attribute::STREET_NAME_NL)] ?? null;
+        if (empty($address['streetname']['fr'])) {
+            $address['streetname']['fr'] = null;
+        }
+        if (empty($address['streetname']['nl'])) {
+            $address['streetname']['nl'] = null;
+        }
+
+        $address['number']['from'] = $attributes[$this->permit_query->contextAttribute(Attribute::STREET_NUMBER_FROM)] ?? null;
+        $address['number']['to'] = $attributes[$this->permit_query->contextAttribute(Attribute::STREET_NUMBER_TO)] ?? null;
+
+        if (empty($address['number']['from'])) {
+            $address['number']['from'] = null;
+        }
+        if (empty($address['number']['to'])) {
+            $address['number']['to'] = null;
+        }
+
+        $address['number']['full'] = (!is_null($address['number']['from']) && !is_null(
+                $address['number']['to']
+            )) ? $address['number']['from'].'-'.$address['number']['to'] : $address['number']['from'];
+
+        $address['municipality']['fr'] = $attributes[$this->permit_query->contextAttribute(Attribute::MUNICIPALITY_FR)];
+        $address['municipality']['nl'] = $attributes[$this->permit_query->contextAttribute(Attribute::MUNICIPALITY_NL)];
+
+        $address['zipcode'] = (int)$attributes['zipcode'];
+
+        return $address;
+    }
+
+
+    public function defineSource($reference_nova): array
+    {
+        if ($this->permit_query->type === 'PE') {
+            $source = [
+                'base_path' => $this->permit_query::PE_PATH,
+                'layer_name' => $this->permit_query::PE_LAYER_NAME,
+            ];
+            $source['query_url'] = $source['base_path'].'?service=WFS&version=2.0.0&request=GetFeature&typeName='.$source['layer_name'].'&outputFormat=application%2Fjson&count=1&cql_filter=ref_nova=\''.$reference_nova.'\'';
+        } else {
+            $source = [
+                'base_path' => $this->permit_query::PU_PATH,
+                'layer_name' => $this->permit_query::PU_LAYER_NAME,
+            ];
+            $source['query_url'] = $source['base_path'].'?service=WFS&version=2.0.0&request=GetFeature&typeName='.$source['layer_name'].'&outputFormat=application%2Fjson&count=1&cql_filter=refnova=\''.$reference_nova.'\'';
+        }
+
+        return $source;
+    }
+
+
+    private function defineSuspensions(?string $json_suspensions): array
+    {
+        $suspensions = [];
+
+        if(is_null($json_suspensions)) {
+            return $suspensions;
+        }
+
+        $array_suspensions = json_decode($json_suspensions, true);
+
+        foreach($array_suspensions as $suspension) {
+            $suspensions['fr'] = $suspension[0]['suspension']['motif-fr'];
+            $suspensions['nl'] = $suspension[0]['suspension']['motif-nl'];
+            $suspensions['from'] = DateTime::createFromFormat('Y-m-d', $suspension[0]['suspension']['date-from'], new DateTimeZone('Europe/Brussels'));
+            $suspensions['to'] = DateTime::createFromFormat('Y-m-d', $suspension[0]['suspension']['date-to'], new DateTimeZone('Europe/Brussels'));
+        }
+
+        return $suspensions;
     }
 }
--- src/PermitQuery.php
+++ src/PermitQuery.php
@@ -8,17 +8,17 @@ use ici\ici_tools\WfsLayer;
 
 class PermitQuery
 {
-    private const PU_PATH = 'https://geoservices-others.irisnet.be/geoserver/Nova/ows';
-    private const PE_PATH = self::PU_PATH;
-    private const PU_LAYER_NAME = 'Nova:vmnovaurbanview';
-    private const PE_LAYER_NAME = 'Nova:vm_nova_pe';
+    public const PU_PATH = 'https://geoservices-others.irisnet.be/geoserver/Nova/ows';
+    public const PE_PATH = self::PU_PATH;
+    public const PU_LAYER_NAME = 'Nova:vmnovaurbanview';
+    public const PE_LAYER_NAME = 'Nova:vm_nova_pe';
 
-    private string $path;
-    private string $layer;
-    private string $cql_filter;
+    public string $path;
+    public string $layer;
+    public string $cql_filter;
     private array $results;
-    private string $type;
-    private int $limit = 1000;
+    public string $type;
+    public int $limit = 1000;
     private array $order;
     public PermitCollection $permits;
 
@@ -111,67 +111,12 @@ class PermitQuery
         return $this;
     }
 
-    /**
-     * @throws \JsonException
-     */
-    public function getResults(): PermitCollection
-    {
-        $wfs = new WfsLayer($this->path, $this->layer);
-        $wfs->setCqlFilter($this->cql_filter)
-            ->setCount($this->limit);
-
-        if(!empty($this->order)) {
-           $wfs->setSortBy($this->order[0], $this->order[1]);
-        }
-
-        $results = $wfs->getPropertiesArray(false);
-
-        foreach ($results as $result) {
-            $permit = new Permit($result[$this->contextAttribute(Attribute::REFERENCE_NOVA)]);
-            $permit->setLanguage($result[$this->contextAttribute(Attribute::LANGUAGE)]);
-            $permit->setType($this->type);
-            //$permit->setSubtype($result[$this->contextAttribute(Attribute::SUBTYPE)]);
-            $permit->setDateSubmission(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_SUBMISSION)]));
-            $permit->setDateArc(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_ARC)]));
-            $permit->setDateAri(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_ARI)]));
-            $permit->setDateAdditionalElements(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_ADDITIONAL_ELEMENTS)]));
-            $permit->setDateCc(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_CC)]));
-            $permit->setDateInquiryBegin(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_INQUIRY_BEGIN)]));
-            $permit->setDateNotification(self::toDatetime($result[$this->contextAttribute(Attribute::DATE_NOTIFICATION)]));
-            $permit->setAreaTypology($this->defineAreaTypologyFromAttributes($result));
-            $permit->setAdvices($this->defineAdvicesFromAttributes($result));
-            $permit->setAddress($this->defineAddressFromAttributes($result));
-            $permit->setSource($this->defineSource($permit->getReferenceNova()));
-            $permit->setSuspensions($this->defineSuspensions($result['suspensions'] ?? null));
-
-            $this->permits->addPermit($permit);
-        }
-
-        return $this->permits;
-    }
-
-    public function first(): ?Permit
-    {
-        return $this->results[0] ?? null;
-    }
-
-    public function all(): array
-    {
-        return $this->results;
-    }
-
-    public function count(): int
-    {
-        return count($this->results) ?? 0;
-    }
-
-    private function contextAttribute(Attribute $attribute): string
+    public function contextAttribute(Attribute $attribute): string
     {
         return $this->type === "PU" ? $attribute->pu() : $attribute->pe();
     }
 
-
-    private static function toDatetime(?string $date): ?DateTime {
+    public static function toDatetime(?string $date): ?DateTime {
         if (is_null($date)) {
             return null;
         }
@@ -184,127 +129,4 @@ class PermitQuery
         return $date_time;
     }
 
-    private function defineAreaTypologyFromAttributes(array $attributes): array
-    {
-        if ($this->type === "PE") {
-            return [];
-        }
-        $typology = [];
-
-        foreach ($attributes as $k => $v) {
-            if (!is_null($v) && (str_contains($k, 'autorized') || str_contains($k, 'existing') || str_contains(
-                        $k,
-                        'projected'
-                    ))) {
-                $type = str_replace(['autorized', 'existing', 'projected'], '', $k);
-                $subtype = str_replace($type, '', $k);
-                $typology[$type][$subtype] = $v;
-            }
-        }
-
-        return $typology;
-    }
-
-    /**
-     * @throws \JsonException
-     */
-    private function defineAdvicesFromAttributes(array $attributes): array
-    {
-        $advices = [];
-
-        $advices['college'] = $attributes['avis_cbe'] ?? $attributes['aviscbe'];
-        $advices['cc'] = $attributes['avis_cc'] ?? $attributes['aviscc'];
-        $advices['fd'] = $attributes['avis_fd'] ?? $attributes['avisfd'];
-
-
-        $json_advices = $attributes['avis_instances'] ?? null;
-        if (is_null($json_advices)) {
-            return $advices;
-        }
-        $json_advices = json_decode($json_advices, true, 512, JSON_THROW_ON_ERROR);
-
-        $instances['fr'] = [];
-        $instances['nl'] = [];
-
-        foreach ($json_advices['data']['Case_Advice_Instance_List']['elements'] as $instance) {
-            $instances['fr'][] = $instance['translations'][0]['label'];
-            $instances['nl'][] = $instance['translations'][1]['label'];
-        }
-
-        return $advices['instances'] = $instances;
-    }
-
-    public function defineAddressFromAttributes(array $attributes): array
-    {
-        $address['streetname']['fr'] = $attributes[$this->contextAttribute(Attribute::STREET_NAME_FR)] ?? null;
-        $address['streetname']['nl'] = $attributes[$this->contextAttribute(Attribute::STREET_NAME_NL)] ?? null;
-        if (empty($address['streetname']['fr'])) {
-            $address['streetname']['fr'] = null;
-        }
-        if (empty($address['streetname']['nl'])) {
-            $address['streetname']['nl'] = null;
-        }
-
-        $address['number']['from'] = $attributes[$this->contextAttribute(Attribute::STREET_NUMBER_FROM)] ?? null;
-        $address['number']['to'] = $attributes[$this->contextAttribute(Attribute::STREET_NUMBER_TO)] ?? null;
-
-        if (empty($address['number']['from'])) {
-            $address['number']['from'] = null;
-        }
-        if (empty($address['number']['to'])) {
-            $address['number']['to'] = null;
-        }
-
-        $address['number']['full'] = (!is_null($address['number']['from']) && !is_null(
-                $address['number']['to']
-            )) ? $address['number']['from'].'-'.$address['number']['to'] : $address['number']['from'];
-
-        $address['municipality']['fr'] = $attributes[$this->contextAttribute(Attribute::MUNICIPALITY_FR)];
-        $address['municipality']['nl'] = $attributes[$this->contextAttribute(Attribute::MUNICIPALITY_NL)];
-
-        $address['zipcode'] = (int)$attributes['zipcode'];
-
-        return $address;
-    }
-
-
-    public function defineSource($reference_nova): array
-    {
-        if ($this->type === 'PE') {
-            $source = [
-                'base_path' => self::PE_PATH,
-                'layer_name' => self::PE_LAYER_NAME,
-            ];
-            $source['query_url'] = $source['base_path'].'?service=WFS&version=2.0.0&request=GetFeature&typeName='.$source['layer_name'].'&outputFormat=application%2Fjson&count=1&cql_filter=ref_nova=\''.$reference_nova.'\'';
-        } else {
-            $source = [
-                'base_path' => self::PU_PATH,
-                'layer_name' => self::PU_LAYER_NAME,
-            ];
-            $source['query_url'] = $source['base_path'].'?service=WFS&version=2.0.0&request=GetFeature&typeName='.$source['layer_name'].'&outputFormat=application%2Fjson&count=1&cql_filter=refnova=\''.$reference_nova.'\'';
-        }
-
-        return $source;
-    }
-
-
-    private function defineSuspensions(?string $json_suspensions): array
-    {
-        $suspensions = [];
-
-        if(is_null($json_suspensions)) {
-            return $suspensions;
-        }
-
-        $array_suspensions = json_decode($json_suspensions, true);
-
-        foreach($array_suspensions as $suspension) {
-            $suspensions['fr'] = $suspension[0]['suspension']['motif-fr'];
-            $suspensions['nl'] = $suspension[0]['suspension']['motif-nl'];
-            $suspensions['from'] = DateTime::createFromFormat('Y-m-d', $suspension[0]['suspension']['date-from'], new DateTimeZone('Europe/Brussels'));
-            $suspensions['to'] = DateTime::createFromFormat('Y-m-d', $suspension[0]['suspension']['date-to'], new DateTimeZone('Europe/Brussels'));
-        }
-
-        return $suspensions;
-    }
 }
